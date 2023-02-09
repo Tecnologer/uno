ffbf294ee87a8bd606b59e5d9f558e01bac00b38
Rey David <rdominguez@tecnologer.net>
2023-02-08 22:55:18 -0600

    move to src game

---
+++ src/game/uno.go
@@ -0,0 +1,24 @@
+package game
+
+import (
+	"log"
+
+	"github.com/tecnologer/uno/src/engine"
+)
+
+const (
+	leftDirection  = "left"
+	rigthDirection = "rigth"
+)
+
+func StartGame(game engine.Game) {
+	firstCard, firstPlayer, err := game.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	game.SetDirection(leftDirection)
+	game.Shuffle(0)
+	game.PlayCard(firstPlayer, firstCard)
+	game.SetNextPlayer(firstPlayer)
+}
