185520efe5b7b143f7c9d4c26cc46fd7f8e4e9de
Rey David <rdominguez@tecnologer.net>
2023-02-08 22:56:01 -0600

    cli (WIP): load plugins

---
+++ cmd/cli/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/tecnologer/uno/plugins"
+	"github.com/tecnologer/uno/src/engine"
+)
+
+var (
+	options = []string{
+		"List games",
+		"Select Game",
+		"Play",
+	}
+
+	availableGames []engine.Game
+)
+
+func main() {
+	err := loadAvailableGames()
+	if err != nil {
+		log.Fatal("loading available games", err)
+	}
+
+	displayOptions()
+}
+
+func loadAvailableGames() (err error) {
+	availableGames, err = plugins.Load()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func displayOptions() {
+	for i, option := range options {
+		fmt.Printf("%d. %s\n", i+1, option)
+	}
+
+	selectOptions()
+}
+
+func selectOptions() {
+	var option int
+	_, e := fmt.Scanf("%d", &option)
+	if e != nil {
+		fmt.Printf("invalid option (%v). try again.", e)
+		selectOptions()
+		return
+	}
+
+	if option < 1 || option >= len(options) {
+		fmt.Printf("invalid option. try again.")
+		selectOptions()
+		return
+	}
+
+	switch options[option-1] {
+	case "List games":
+		displayAvailableGames()
+	}
+}
+
+func displayAvailableGames() {
+	for _, game := range availableGames {
+		fmt.Printf("\t- %s\n", game.GetVersionName())
+	}
+}
