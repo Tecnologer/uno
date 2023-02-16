cbedd42907043065f62e8c1ecdde4a450d737a9e
tecnologer <rdominguez@tecnologer.net>
2023-02-15 21:16:39 -0600

    game: update type of direction  and result of turn

--- versions/classic/game.go
+++ versions/classic/game.go
@@ -7,21 +7,28 @@ import (
 	"github.com/tecnologer/uno/src/tools/cards"
 )
 
+type direction string
+
 const (
-	left2Rigth = "left_rigth"
-	rigth2Left = "rigth_left"
+	left2Rigth direction = "left_rigth"
+	rigth2Left direction = "rigth_left"
+
+	cardsPerPlayer int = 7
 )
+
+func (d direction) IsLeft() bool {
+	return d == left2Rigth
+}
 
 type classic struct {
 	metadata      metadata
-	direction     string
+	direction     direction
 	currentColor  string
 	currentPlayer engine.Player
 	playerSaidUno engine.Player
 	drawPile      []engine.Card
 	discardedPile []engine.Card
 	players       []engine.Player
-	output        chan engine.Result
 }
 
 func New() classic {
@@ -32,21 +39,15 @@ func (c *classic) GetMetadata() engine.Metadata {
 	return c.metadata
 }
 
-func (c *classic) New() (_ chan engine.Result, err error) {
-	if c.output != nil {
-		close(c.output)
-	}
-
+func (c *classic) New() (err error) {
 	c.drawPile, err = loadCards()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	c.direction = rigth2Left
 
-	c.output = make(chan engine.Result)
-
-	return c.output, nil
+	return nil
 }
 
 func (c *classic) Start() (engine.Card, engine.Player, error) {
@@ -75,6 +76,43 @@ func (g *classic) GetDrawPile() []engine.Card {
 	return g.drawPile
 }
 
+func (c *classic) AddPlayer(playerName string) error {
+	if c.isPlayerPlaying(playerName) {
+		return fmt.Errorf("the player %s is in the game", playerName)
+	}
+
+	player := &player{
+		name:   playerName,
+		output: make(chan engine.TurnResult, 1),
+		cards:  c.getCardsForPlayer(),
+		index:  len(c.players),
+	}
+
+	c.players = append(c.players, player)
+
+	return nil
+}
+
+func (c *classic) isPlayerPlaying(playerName string) bool {
+	for _, player := range c.players {
+		if player.GetName() == playerName {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (c *classic) getCardsForPlayer() []engine.Card {
+	cards := make([]engine.Card, cardsPerPlayer)
+
+	for i := range cards {
+		cards[i] = c.DrawCard()
+	}
+
+	return cards
+}
+
 func (c *classic) GetPlayers() []engine.Player {
 	return c.players
 }
@@ -87,23 +125,76 @@ func (c *classic) SetNextPlayer(nextPlayer engine.Player) {
 	c.currentPlayer = nextPlayer
 }
 
-func (c *classic) PlayCard(player engine.Player, card engine.Card) error {
+func (c *classic) PlayCard(player engine.Player, card engine.Card) (engine.TurnResult, error) {
 	result, err := card.Play(c.currentColor, c.discardedPile)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	c.output <- result
+	c.updateDirection(result.IsReverse())
 
-	return nil
+	r := &turnResult{
+		card:      card,
+		player:    c.getNextPlayer(player, result.IsReverse(), result.IsSkip()),
+		direction: c.direction,
+	}
+
+	return r, nil
 }
 
-func (c *classic) GetDirection() string {
+func (c *classic) getNextPlayer(currentPlayer engine.Player, isReverse, isSkip bool) engine.Player {
+	var (
+		nextPlayer      engine.Player
+		nextPlayerIndex int
+	)
+
+	if isReverse {
+		nextPlayerIndex = currentPlayer.GetIndex() - 1
+	} else {
+		nextPlayerIndex = currentPlayer.GetIndex() + 1
+	}
+
+	if nextPlayerIndex < 0 {
+		nextPlayerIndex = len(c.players) - 1
+	}
+
+	if nextPlayerIndex >= len(c.players) {
+		nextPlayerIndex = 0
+	}
+
+	if isSkip {
+		nextPlayerIndex++
+	}
+
+	nextPlayer = c.players[nextPlayerIndex]
+
+	return nextPlayer
+}
+
+func (c *classic) updateDirection(isReverse bool) {
+	if !isReverse {
+		return
+	}
+
+	if c.direction == left2Rigth {
+		c.direction = rigth2Left
+		return
+	}
+
+	c.direction = left2Rigth
+}
+
+func (c *classic) GetDirection() engine.Direction {
 	return c.direction
 }
 
-func (c *classic) SetDirection(direction string) {
-	c.direction = direction
+func (c *classic) SetDirection(d engine.Direction) {
+	if d.IsLeft() {
+		c.direction = left2Rigth
+		return
+	}
+
+	c.direction = rigth2Left
 }
 
 func (c *classic) SayUno(player engine.Player) {
@@ -114,6 +205,6 @@ func (c *classic) GetDiscardedPile() []engine.Card {
 	return c.discardedPile
 }
 
-func (c *classic) Close() {
-	close(c.output)
-}
+// func (c *classic) Close() {
+// 	close(c.output)
+// }
--- versions/classic/game_test.go
+++ versions/classic/game_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"reflect"
 	"testing"
 
 	"github.com/tecnologer/uno/engine"
@@ -62,4 +63,197 @@ func Test_classic_Shuffle(t *testing.T) {
 			}
 		})
 	}
+}
+
+func Test_classic_getNextPlayer(t *testing.T) {
+	players := getPlayersForTests()
+
+	type args struct {
+		currentPlayer engine.Player
+		isReverse     bool
+		isSkip        bool
+	}
+	tests := []struct {
+		name    string
+		players []engine.Player
+		args    args
+		want    engine.Player
+	}{
+		{
+			name:    "normal",
+			players: players,
+			args: args{
+				currentPlayer: players[1],
+				isReverse:     false,
+				isSkip:        false,
+			},
+			want: players[2],
+		},
+		{
+			name:    "skip",
+			players: players,
+			args: args{
+				currentPlayer: players[0],
+				isReverse:     false,
+				isSkip:        true,
+			},
+			want: players[2],
+		},
+		{
+			name:    "skip_last",
+			players: players,
+			args: args{
+				currentPlayer: players[len(players)-1],
+				isReverse:     false,
+				isSkip:        true,
+			},
+			want: players[1],
+		},
+		{
+			name:    "reverse",
+			players: players,
+			args: args{
+				currentPlayer: players[2],
+				isReverse:     true,
+				isSkip:        false,
+			},
+			want: players[1],
+		},
+		{
+			name:    "reverse_first",
+			players: players,
+			args: args{
+				currentPlayer: players[0],
+				isReverse:     true,
+				isSkip:        false,
+			},
+			want: players[len(players)-1],
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &classic{
+				players: tt.players,
+			}
+			if got := c.getNextPlayer(tt.args.currentPlayer, tt.args.isReverse, tt.args.isSkip); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("classic.getNextPlayer() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_classic_updateDirection(t *testing.T) {
+	type args struct {
+		isReverse bool
+	}
+	tests := []struct {
+		name      string
+		direction direction
+		args      args
+		want      direction
+	}{
+		{
+			name:      "left_to_rigth_normal",
+			direction: left2Rigth,
+			args: args{
+				isReverse: false,
+			},
+			want: left2Rigth,
+		},
+		{
+			name:      "left_to_rigth_reverse",
+			direction: left2Rigth,
+			args: args{
+				isReverse: true,
+			},
+			want: rigth2Left,
+		},
+		{
+			name:      "rigth_to_left_normal",
+			direction: rigth2Left,
+			args: args{
+				isReverse: false,
+			},
+			want: rigth2Left,
+		},
+		{
+			name:      "rigth_to_left_reverse",
+			direction: rigth2Left,
+			args: args{
+				isReverse: true,
+			},
+			want: left2Rigth,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &classic{
+				direction: tt.direction,
+			}
+			c.updateDirection(tt.args.isReverse)
+
+			if c.direction != tt.want {
+				t.Errorf("classic.updateDirection() = %s, want = %s", c.direction, tt.want)
+			}
+		})
+	}
+}
+
+func Test_classic_PlayCard(t *testing.T) {
+	type fields struct {
+		direction     direction
+		currentColor  string
+		currentPlayer engine.Player
+		playerSaidUno engine.Player
+		drawPile      []engine.Card
+		discardedPile []engine.Card
+		players       []engine.Player
+	}
+	type args struct {
+		player engine.Player
+		card   engine.Card
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    engine.TurnResult
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &classic{
+				metadata:      tt.fields.metadata,
+				direction:     tt.fields.direction,
+				currentColor:  tt.fields.currentColor,
+				currentPlayer: tt.fields.currentPlayer,
+				playerSaidUno: tt.fields.playerSaidUno,
+				drawPile:      tt.fields.drawPile,
+				discardedPile: tt.fields.discardedPile,
+				players:       tt.fields.players,
+			}
+			got, err := c.PlayCard(tt.args.player, tt.args.card)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("classic.PlayCard() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("classic.PlayCard() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func getPlayersForTests() []engine.Player {
+	players := make([]engine.Player, 5)
+	for i := range players {
+		players[i] = &player{
+			name:  fmt.Sprintf("Player_%d", i),
+			index: i,
+		}
+	}
+
+	return players
 }
